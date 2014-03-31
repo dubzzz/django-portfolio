8deef91793eaeb3cbe1c3781dbe0712d0992eead
Nicolas DUBIEN <github@dubien.org>
2014-03-31 19:17:57 +0200

    Templatetags improvments
    'url_to_link' has been renamed 'escape_project'
    
    It can:
    * Find URLs in a RAW text and change them to link
    * Recognize a list (r"\n* [^\n]+")

--- PortFolio/projects/templatetags/project_extras.py
+++ PortFolio/projects/templatetags/project_extras.py
@@ -9,17 +9,47 @@ import re
 register = template.Library()
 
 @register.filter(is_safe=True)
-def url_to_link(text):
+def escape_project(text):
     """
     This templatetag converts every url in text to an hyperlink.
     The text is escaped for HTML before adding hyperlinks.
 
+    URL to link
+    -----------
     INPUT:
         Please visit: http://portfolio.dubien.me/
     OUTPUT:
-        Please visit: <a href="http://portfolio.dubien.me/" target="blank_">http://portfolio.dubien.me/</a>
+        <p>Please visit: <a href="http://portfolio.dubien.me/" target="blank_">http://portfolio.dubien.me/</a></p>
+
+    Bulletpoints to list
+    --------------------
+    INPUT:
+        This is a list:
+        * element 1
+        * element 2
+    OUTPUT:
+        <p>This is a list:</p><ul><li>element 1</li><li>element 2</li></ul>
     """
     
     escaped_text = escape(text)
-    return mark_safe(re.sub(r'(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', '<a href="\g<url>" target="blank_">\g<url></a>', escaped_text))
+
+    # URL to link
+
+    escaped_text = re.sub(r'(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', '<a href="\g<url>" target="blank_">\g<url></a>', escaped_text)
+    
+    # Bulletpoints to list
+    
+    escaped_text = re.sub(r'\n\*\s(?P<li_element>[^\n]+)', '</p><ul><li>\g<li_element></li></ul><p>', escaped_text).replace('</ul><p></p><ul>', '')
+    
+    if escaped_text.endswith("<p>"):
+        escaped_text = escaped_text[:-3]
+    else:
+        escaped_text += "</p>"
+    
+    if escaped_text.startswith("</p>"):
+        escaped_text = escaped_text[4:]
+    else:
+        escaped_text = "<p>%s" % escaped_text
+    
+    return mark_safe(escaped_text)
 
