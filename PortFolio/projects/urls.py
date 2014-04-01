63e71a72998c24fb0882dcf7fd10e0891c37a683
Nicolas DUBIEN <github@dubien.org>
2014-04-02 00:34:44 +0200

    Add WYSIWYG Admin to update already existing 'description'

--- PortFolio/projects/urls.py
+++ PortFolio/projects/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('projects.views',
     url('^$', 'home'),
     url('^edit/project/(?P<project_id>\d+)/add/(?P<description_type>\w+)/$', 'add_description_to'),
+    url('^edit/description/(?P<description_id>\d+)/$', 'update_description'),
     url('^project/(?P<project_url>[^/]+)/$', 'show_project'),
 )
 
--- PortFolio/projects/views.py
+++ PortFolio/projects/views.py
@@ -42,7 +42,7 @@ def add_description_to(request, project_id, description_type):
     
     project = get_object_or_404(Project, pk=project_id)
     
-    # Check that the request hab been transmitted in POST
+    # Check that the request has been transmitted in POST
     if request.method == 'POST':
         form = None
         
@@ -61,4 +61,34 @@ def add_description_to(request, project_id, description_type):
             description_elt.save()
     
     return HttpResponseRedirect(reverse('projects.views.show_project', args=[project.name_url]))
+
+@login_required
+def update_description(request, description_id):
+    """
+    Modify/Change an existing description
+    """
+
+    description = get_object_or_404(Description, pk=description_id)
+    
+    # Check that the request has been transmitted in POST
+    if request.method == 'POST':
+        form = None
+        real_description = description.cast()
+        
+        # Ajustments depending on the kind of description
+        if isinstance(real_description, RawTextDescription):
+            form = RawTextDescriptionForm(request.POST, instance=real_description)
+        elif isinstance(real_description, HtmlCodeDescription):
+            form = HtmlCodeDescriptionForm(request.POST, instance=real_description)
+        elif isinstance(real_description, ImageDescription):
+            form = ImageDescriptionForm(request.POST, instance=real_description)
+
+        # Check form validity
+        if form and form.is_valid():
+            real_description = form.save(commit=False)
+            if request.FILES['image'] and isinstance(real_description, ImageDescription):
+                real_description.image = request.FILES['image']
+            real_description.save()
+
+    return HttpResponseRedirect(reverse('projects.views.show_project', args=[description.project.name_url]))
 
