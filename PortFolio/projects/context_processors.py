77f9282cae653c81e47ad0507a123bf34b264ec1
Nicolas DUBIEN <github@dubien.org>
2014-04-20 02:22:38 +0200

    Add authorship (for Google Plus profiles)

--- PortFolio/projects/context_processors.py
+++ PortFolio/projects/context_processors.py
@@ -4,6 +4,7 @@ from django.conf import settings
 def my_context_processor(request):
     return {
             "HEADER_ADMIN": settings.HEADER_ADMIN,
+            "HEADER_GPLUS": settings.HEADER_GPLUS,
             "HEADER_QUICKLINKS": settings.HEADER_QUICKLINKS,
             "FOOTER_TEXT": settings.FOOTER_TEXT,
             "FOOTER_QUICKLINKS": settings.FOOTER_QUICKLINKS,
