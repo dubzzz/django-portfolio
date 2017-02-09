0cccf01a3df41f545ed4f514c6f224fc4e39c077
Nicolas DUBIEN <github@dubien.org>
2017-02-09 22:00:33 +0100

    Update generate_db script

--- www/scripts/generate_db.py
+++ www/scripts/generate_db.py
@@ -14,30 +14,36 @@ import hashlib
 import getpass
 import sys
 from os import path, urandom
+
 SCRIPT_PATH = path.dirname(__file__)
 DEFAULT_DB = path.join(SCRIPT_PATH, "../projects.db")
 
+CURRENT_VERSION = 0
+
+def _generate_table_users(c):
+    c.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT NOT NULL,
+                    password TEXT NOT NULL,
+                    salt TEXT NOT NULL)''')
+
+def _generate_table_updates(c):
+    c.execute('''CREATE TABLE IF NOT EXISTS updates (
+                   name VARCHAR(20) NOT NULL UNIQUE,
+                   revision INTEGER NOT NULL)''')
+
 def generate_tables():
-    conn = sqlite3.connect(DEFAULT_DB)
-    
-    with conn:
+    with sqlite3.connect(DEFAULT_DB) as conn:
         c = conn.cursor()
         
-        # Drop tables if they exist
-        #c.execute('''DROP TABLE IF EXISTS node''')
-        #c.execute('''DROP TABLE IF EXISTS expense''')
-        #c.execute('''DROP TABLE IF EXISTS node_expense''')
-        #c.execute('''DROP TABLE IF EXISTS users''')
+        _generate_table_users(c)
+        _generate_table_updates(c)
         
-        # Create tables
-        c.execute('''CREATE TABLE IF NOT EXISTS users (
-                        id INTEGER PRIMARY KEY,
-                        username TEXT NOT NULL,
-                        password TEXT NOT NULL,
-                        salt TEXT NOT NULL)''')
-        
-        # Commit the changes
+        c.execute('''INSERT INTO updates (name, revision) VALUES ('__global__', ?)''', (CURRENT_VERSION,))
         conn.commit()
+
+def migrate_from_django():
+    generate_tables()
 
 def add_administrator():
     if sys.version_info < (3, 0):
@@ -52,8 +58,8 @@ def add_administrator():
         if password != cpassword:
             ask_password = True
             print("Passwords differ")
-    conn = sqlite3.connect(DEFAULT_DB)
-    with conn:
+    
+    with sqlite3.connect(DEFAULT_DB) as conn:
         c = conn.cursor()
         salt = urandom(16).encode('hex')
         h = hashlib.sha1()
@@ -61,10 +67,20 @@ def add_administrator():
         hashvalue= h.hexdigest()
         c.execute('''DELETE FROM users WHERE username=?''', (username,))
         c.execute('''INSERT INTO users (username, password, salt)
-                        VALUES (?, ?, ?)''', (username, hashvalue, salt));
+                        VALUES (?, ?, ?)''', (username, hashvalue, salt))
+        conn.commit()
 
 
 if __name__ == '__main__':
-    generate_tables()
-    add_administrator()
+    if len(sys.argv) < 2:
+        print("Syntax error: takes at least one parameter (init/migrate/adduser)")
+        exit(1)
+    
+    op = sys.argv[1]
+    if op == "init":
+        generate_tables()
+    elif op == "migrate":
+        migrate_from_django()
+    elif op == "adduser":
+        add_administrator()
 
