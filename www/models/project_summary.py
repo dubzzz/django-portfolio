07c17d18b1ade55e8bfc74efcb32027363c90c92
Nicolas DUBIEN <github@dubien.org>
2017-02-10 01:19:44 +0100

    Render the per year page

--- www/models/project_summary.py
+++ www/models/project_summary.py
@@ -15,6 +15,14 @@ class ProjectSummary:
         self.short_description = short_description
         self.private = private
 
+def project_from_query(data):
+    year = data[0]
+    name_url = data[1]
+    name = data[2]
+    short_description = data[3]
+    private = data[4]
+    return ProjectSummary(year, name_url, name, short_description, private)
+
 def load_all_summaries(private_only):
     year_mapping = dict()
     projects_by_year = list();
@@ -25,17 +33,39 @@ def load_all_summaries(private_only):
         else:
             c.execute('''SELECT year, name_url, name, short_description, private FROM projects_project''')
         for data in c.fetchall():
-            year = data[0]
-            name_url = data[1]
-            name = data[2]
-            short_description = data[3]
-            private = data[4]
+            p = project_from_query(data)
+            if p.year not in year_mapping:
+                year_mapping[p.year] = len(projects_by_year)
+                projects_by_year.append((p.year, list(),))
             
-            if year not in year_mapping:
-                year_mapping[year] = len(projects_by_year)
-                projects_by_year.append((year, list(),))
-            
-            projects_by_year[year_mapping[year]][1].append(ProjectSummary(year, name_url, name, short_description, private))
+            projects_by_year[year_mapping[p.year]][1].append(p)
     
     return sorted(projects_by_year, key=lambda projects: projects[0], reverse=True)
+
+def load_summaries_of_year(year, private_only):
+    projects = list();
+    with sqlite3.connect(DEFAULT_DB) as conn:
+        c = conn.cursor()
+        if private_only:
+            c.execute('''SELECT year, name_url, name, short_description, private FROM projects_project WHERE year=? AND private=?''', (year, False,))
+        else:
+            c.execute('''SELECT year, name_url, name, short_description, private FROM projects_project WHERE year=?''', (year,))
+        for data in c.fetchall():
+            projects.append(project_from_query(data))
+    return projects
+
+def surround_years(year, private_only):
+    prev_year, next_year = None, None
+    with sqlite3.connect(DEFAULT_DB) as conn:
+        c = conn.cursor()
+        if private_only:
+            c.execute('''SELECT MAX(year) as prev_year, NULL as next_year FROM projects_project WHERE year<? AND private=? UNION SELECT NULL as prev_year, MIN(year) as next_year FROM projects_project WHERE year>? AND private=?''', (year, False, year, False))
+        else:
+            c.execute('''SELECT MAX(year) as prev_year, NULL as next_year FROM projects_project WHERE year<? UNION SELECT NULL as prev_year, MIN(year) as next_year FROM projects_project WHERE year>?''', (year,year,))
+        for data in c.fetchall():
+            if data[0] is not None:
+                prev_year = data[0]
+            if data[1] is not None:
+                next_year = data[1]
+    return (prev_year, next_year)
 
--- www/views/projects.py
+++ www/views/projects.py
@@ -8,17 +8,19 @@ __CURRENT_PATH = path.dirname(__file__)
 from auth import BaseHandler
 
 sys.path.append(path.join(__CURRENT_PATH, "..", "models"))
-from project_summary import load_all_summaries
+from project_summary import load_all_summaries, load_summaries_of_year, surround_years
 
 class HomeHandler(BaseHandler):
     def get(self):
-      self.render("home.html", STATS=None, by_year_list=load_all_summaries(self.is_authentificated()))
+        self.render("home.html", STATS=None, by_year_list=load_all_summaries(self.is_authentificated()))
 
 class PerYearHandler(BaseHandler):
     def get(self, year):
-      self.render("base.html")
+        projects=load_summaries_of_year(year, self.is_authentificated())
+        prev_year, next_year = surround_years(year, self.is_authentificated())
+        self.render("projects_year.html", projects=projects, year=year, prev_year=prev_year, next_year=next_year)
 
 class ProjectHandler(BaseHandler):
     def get(self, year, project):
-      self.render("base.html")
+        self.render("base.html")
 
